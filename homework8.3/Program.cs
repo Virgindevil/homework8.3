89acf55b1569599f41a247c3c8b1acf446f68ca5
S.Matuzov <98310589+Virgindevil@users.noreply.github.com>
2022-03-18 16:16:30 +0400

    Add files via upload

---
+++ homework8.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            HashSet<int> numbers = new HashSet<int>();
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите число:");
+                    int number = int.Parse(Console.ReadLine());
+                    if (numbers.Contains(number))
+                    {
+                        Console.WriteLine("Число уже было введено.");
+                    }
+                    else
+                    {
+                        numbers.Add(number);
+                        Console.WriteLine("Число успешно добавлено!");
+                    }
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+            }  
+        }
+    }
+}
